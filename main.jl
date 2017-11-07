b5fe24557b762a49ad9586d38843d90421e73c4f
Jesús A. Mejía D <jesusmejded@gmail.com>
2017-11-07 00:49:30 -0600

    base files

---
+++ main.jl
@@ -0,0 +1,61 @@
+using Metaheuristics
+include("tools.jl")
+
+
+
+function myError(parameters, X, ptsPCA, nPCA)
+    b = parameters[7:end]
+    Y = reshape(ptsPCA * b, 20, 2)'
+
+    X_approx = applyTransform(Y, parameters[1:6]) # 2×20
+    
+    return sum((X - X_approx).^2) / 20 
+    # return abs(mean(X_approx) - mean(X)) + abs(std(X_approx) - std(X))
+end
+
+function main()
+    # imTarget = imgLoad("faces/me.jpg")
+    # ptsTarget= readdlm("csv/me.csv", Float64)
+
+    imTarget, ptsTarget = opendist(603)
+    # read pts and create matrix
+    mat_pts = createMatrix()
+
+    # Calculate PCs
+    ptsPCA = myPCA(mat_pts)
+    nPCA = size(ptsPCA, 2)
+
+    X = ptsTarget'
+
+    plotdist(imTarget, X, "Target distribution")
+
+    η   = 5.0
+    lims= (-10, 10)
+    D   = 6 + nPCA
+    nEvals = 20000D
+
+    fitnessFunc(parms) = myError(parms, X, ptsPCA, nPCA)
+
+    nruns = 5
+
+    for _ = 1:nruns
+        @time approxParms, ee = eca(fitnessFunc, D; η_max = η,
+                                                limits = lims,
+                                                termination= x->std(x) < 1e-15,
+                                                max_evals  = nEvals,
+                                                correctSol=false)
+        
+        b = approxParms[7:end]
+        Y = reshape(ptsPCA * b, 20, 2)'
+        
+        X_approx = applyTransform(Y, approxParms)
+        plotdist(imTarget, X_approx, "approx distribution", :r)
+        println(approxParms)
+
+    end
+
+    
+
+end
+
+main()
---
+++ tools.jl
@@ -0,0 +1,69 @@
+using MultivariateStats
+include("/home/jesus/develop/repos/image-analysis/tools.jl")
+
+function myPCA(x)
+    M = PCA
+    pca_ = fit(PCA, x)
+
+    p = principalvars(pca_) / tvar(pca_)
+    println("PC explanation: ", p)
+    println("PCA %: ", sum(p))
+
+    return projection(pca_)
+end
+
+function createMatrix(numFaces = 1521, npts = 20)
+    D = 2npts
+    myMatrix = zeros(Float64, D, numFaces)
+
+    for i = 1:numFaces
+        # 2×20
+        pts = openpts(i-1)
+        myMatrix[:,i] = reshape(pts, D)
+    end
+
+    return myMatrix
+end
+
+function savePTS2csv()
+    for i = 0:1520
+        println("file num. $i")
+        inputName  = @sprintf("points_20/bioid_%04d.pts", i)
+        outputName = @sprintf("csv/points_%04d.csv", i)
+        
+        pts = readcsv(inputName, String);
+        pts = pts[4:end-1]
+        writecsv(outputName, pts)
+    end
+end
+
+function opendist(i)
+    imname = @sprintf("faces/BioID_%04d.jpg", i)
+    fname = @sprintf("csv/points_%04d.csv", i)        
+    return imgLoad(imname, 1), readdlm(fname, Float64);
+end
+
+function openpts(i)
+    fname = @sprintf("csv/points_%04d.csv", i)        
+    return readdlm(fname, Float64)';
+end
+
+function plotdist(img, pts, mytitle="", mcolor=:b)
+    imshow(img, cmap="gray")
+    # 2×20
+    plot(pts[1,:], pts[2,:], marker=:o, lw=0,markersize=4, color=mcolor)
+    title(mytitle)
+end
+
+function applyTransform(x, parameters)
+
+    # rotation and scale matrix
+    A = [parameters[1] parameters[2];
+         parameters[3] parameters[4]]
+
+    # translate vector
+    t = [parameters[5]; parameters[6]]
+
+    cols = size(x, 2)
+    return A * x + repmat(t, 1, cols)
+end
